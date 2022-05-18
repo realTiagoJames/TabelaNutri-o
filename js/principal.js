8d3b62dbfadbf6300a5084032a8a183b8dc7911f
Tiago Lima <james.lima.devops@gmail.com>
2022-05-18 17:28:03 -0300

    Calculo e imprimir Imc na tabela

--- js/principal.js
+++ js/principal.js
@@ -2,5 +2,31 @@ console.log("Fui carregado de um arquivo externo.");
 
 
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Bruna Nutrição"
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc")
+
+// Calculo imc:
+var imc = peso / (altura * altura);  // 100/2.0 * 2.0 = 100/4 =>>>> 25
+console.log(imc);  //imc
+
+tdImc.textContent = imc;
+
+
+
+
+// Mensagens no console
+console.log(paciente); //tr
+console.log(tdPeso);  //td que tem o peso
+console.log(peso);  //Obter 100
+console.log(tdAltura);  //td que tem o peso
+console.log(altura);  //Obter 100
